331d052451f178d4918571b46cef23502b8c2ba6
feathershine <zhl13779081529@hotmail.com>
2018-05-30 18:58:21 +0800

    改进架构实现发射子弹
    2018年5月30日18:57:45
    
    1、改进游戏框架
    2、优化子弹发射逻辑

--- Assets/Resources/Scripts/Bullet.cs
+++ Assets/Resources/Scripts/Bullet.cs
@@ -7,7 +7,7 @@ namespace FloatBall
     public class Bullet : MonoBehaviour
     {
 
-        public float speed = 10f;
+        float speed = 5.0f;
         Vector3 direction;
 
         public Vector3 Direction
@@ -32,36 +32,14 @@ namespace FloatBall
         void Update()
         {
             transform.Translate(direction * Time.deltaTime * speed);
-            if (Findborder())
+            // 到达边界销毁此对象
+            if (BorderInspector.Onborder(gameObject.transform.position))
             {
                 Destroy(gameObject);
             }
         }
 
-        //检测子弹是否到达游戏边界
-        bool Findborder()
-        {
-            float locx = transform.position.x;
-            float locy = transform.position.y;
-            GameObject itself = gameObject;
-            if (locy > GameManager.GameBorderUp)
-            {
-                return true;
-            }
-            if (locy < GameManager.GameBorderDown)
-            {
-                return true;
-            }
-            if (locx < GameManager.GameBorderLeft)
-            {
-                return true;
-            }
-            if (locx > GameManager.GameBorderRight)
-            {
-                return true;
-            }
-            return false;
-        }
+
     }
 }
 
